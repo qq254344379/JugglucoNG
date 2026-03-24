fe3f33d1daf6d2c1e77f36182f5000a6314404ca
E Y <ctqwaa@gmail.com>
2026-03-24 14:39:31 +0500

    Sensor source resolver

---
+++ Common/src/test/java/tk/glucodata/SensorSourceResolverTests.java
@@ -0,0 +1,22 @@
+package tk.glucodata;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SensorSourceResolverTests {
+    @Test
+    public void resolvesKnownFallbackKindsWithoutSnapshot() {
+        assertEquals("Libre2", SensorSourceResolver.resolveSourceInfo(null, SensorSourceResolver.SENSOR_KIND_LIBRE2));
+        assertEquals("Libre3", SensorSourceResolver.resolveSourceInfo(null, SensorSourceResolver.SENSOR_KIND_LIBRE3));
+        assertEquals("GS1Sb", SensorSourceResolver.resolveSourceInfo(null, SensorSourceResolver.SENSOR_KIND_SIBIONICS));
+        assertEquals("AccuChek", SensorSourceResolver.resolveSourceInfo(null, SensorSourceResolver.SENSOR_KIND_ACCUCHEK));
+        assertEquals("G7", SensorSourceResolver.resolveSourceInfo(null, SensorSourceResolver.SENSOR_KIND_DEXCOM));
+    }
+
+    @Test
+    public void unresolvedFallbackDoesNotLie() {
+        assertEquals("Unknown", SensorSourceResolver.resolveSourceInfo(null, 0));
+        assertEquals("Unknown", SensorSourceResolver.resolveSourceInfo(null, -1));
+    }
+}
